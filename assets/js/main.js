b04de939ebb73b1b006e9b1382d6f8f2f8fcc073
Muhammad Areeb <mareebsheikh92@gmail.com>
2024-12-14 20:29:57 +0500

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,13 @@
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
